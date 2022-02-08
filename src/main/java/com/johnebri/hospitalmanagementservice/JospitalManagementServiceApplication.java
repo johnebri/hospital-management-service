9b31c3cf2f99f5332e68aa71f9950a46250e799b
johnebri <john.ebri@yahoo.com>
2022-02-08 23:50:01 +0100

    (Chore):project setup

---
+++ src/main/java/com/johnebri/hospitalmanagementservice/JospitalManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.johnebri.hospitalmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JospitalManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JospitalManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/johnebri/hospitalmanagementservice/JospitalManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnebri.hospitalmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JospitalManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
